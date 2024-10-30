2f3ac46fb45c8f5afa7b2836a14415890b1bfcc0
Manish <manishjavadeveloper4u@gmail.com>
2024-10-30 15:37:34 +0530

    Initial project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Medical Project',
+    version= '0.0.0',
+    author= 'Manish',
+    author_email= 'manishjavadeveloper4u@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
